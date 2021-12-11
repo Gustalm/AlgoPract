8248c79c93b96abce3050440356daa65d5805f72
gustatec <gustatec>
2021-12-11 14:37:46 +0000

    Add Twosum and Valid Palindrom algs

---
+++ ArraysAndStrings/TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var availableNums = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var valueLeftToTarget = target - nums[i];
+            if (availableNums.TryGetValue(valueLeftToTarget, out var index))
+            {
+                return new int[] { i, index };
+            }
+
+            if (!availableNums.ContainsKey(nums[i]))
+                availableNums.Add(nums[i], i);
+        }
+
+        return new int[2];
+    }
+}
---
+++ ArraysAndStrings/ValidPalindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var aox = new Solution();
+Console.Write(aox.IsPalindrome("A man, a plan, a canal: Panama"));
+public class Solution
+{ 
+//less efficient
+//    public bool IsPalindrome(string s)
+//    {
+//        var regex = new Regex("[^a-zA-Z0-9]");
+//        var onlyAlphaNumericString = regex.Replace(s.ToLower(), "");
+//        var stringReversed = new String(onlyAlphaNumericString.Reverse().ToArray());
+
+
+//        return onlyAlphaNumericString.Equals(stringReversed);
+//    }
+
+    public bool IsPalindrome(string s)
+    {
+        var onlyAlphaNumericString = new String(s.ToLower().Where(x => char.IsLetterOrDigit(x)).ToArray());
+
+        return onlyAlphaNumericString.Equals(new String(onlyAlphaNumericString.Reverse().ToArray()));
+    }
+}
