d194f51af5896dde6c443b296c42071fb9bb4fa0
gustatec <gustatec>
2025-02-15 17:39:57 +0000

    Just some algs

---
+++ Struct/Anagrams/Program.cs
@@ -0,0 +1,31 @@
+﻿static bool anagrams(string s1, string s2)
+{
+    var hashMap1 = new Dictionary<char, int>();
+    var hashMap2 = new Dictionary<char, int>();
+
+    for (int i = 0; i < s1.Length; i++)
+    {
+        if (hashMap1.ContainsKey(s1[i]))
+        {
+            hashMap1[s1[i]]++;
+        }
+        else
+        {
+            hashMap1[s1[i]] = 1;
+        }
+    }
+
+    for (int i = 0; i < s1.Length; i++)
+    {
+        if (hashMap2.ContainsKey(s1[i]))
+        {
+            hashMap2[s1[i]]++;
+        }
+        else
+        {
+            hashMap2[s1[i]] = 1;
+        }
+    }
+
+    return hashMap1.Equals(hashMap2);
+}
---
+++ Struct/MaxValue/Program.cs
@@ -0,0 +1,15 @@
+﻿static double maxValue(double[] numbers)
+{
+    var maxValue = numbers[0];
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        if(maxValue < numbers[i])
+        {
+            maxValue = numbers[i];
+        }
+    }
+    // todo
+    return maxValue;
+}
+
+Console.WriteLine(maxValue([6, 2, 1, 3, 14]));
