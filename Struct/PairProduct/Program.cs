053d6719dbd25fadae7cc9e3f9a577fad889bad3
gustatec <gustatec>
2025-02-23 15:05:35 +0000

    Add algorithms

---
+++ Struct/PairProduct/Program.cs
@@ -0,0 +1,20 @@
+﻿static List<int> pairProduct(List<int> numbers, int target)
+{
+    var hashMap = new Dictionary<int, int>();
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        if (target % numbers[i] != 0)
+        {
+            continue;
+        }
+
+        var numberLeft = target / numbers[i];
+        if (hashMap.ContainsKey(numberLeft)){
+            return [i, hashMap[numberLeft]];
+        }
+
+        hashMap.Add(numbers[i], i);
+    }
+
+    return [];
+}
--- Struct/PairSum/Program.cs
+++ Struct/PairSum/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+static List<int> pairSum(List<int> numbers, int target)
+{
+    var hashMap = new Dictionary<int, int>();
+    for (int i = 0; i < numbers.Count; i++)
+    {
+        var numberLeft = target - numbers[i];
+        if (hashMap.ContainsKey(numberLeft))
+        {
+            return [i, hashMap[numberLeft]];
+        }
+        else
+        {
+            hashMap.Add(numbers[i], i);
+        }
+    }
+
+    return [];
+}
