18efddae2a8cc756354e91755791d22ca877f47e
gustatec <gustatec>
2025-03-18 18:54:55 +0000

    Commit latest algs

---
+++ Struct/LongestWord/Program.cs
@@ -0,0 +1,14 @@
+﻿static string longestWord(string sentence)
+{
+    var words = sentence.Split(' ');
+    var biggestWord = "";
+    for (int i = 0; i <= words.Length; i++)
+    {
+        if (words[i].Length >= biggestWord.Length)
+        {
+            biggestWord = words[i];
+        }
+    }
+
+    return biggestWord;
+}
---
+++ Struct/Uncompress/Program.cs
@@ -0,0 +1,25 @@
+﻿//< number >< char >
+//for example, '2c' or '3a'.
+using System.Text;
+
+static string uncompress(string s)
+{
+    var result = new StringBuilder();
+    var lastNumberIndex = 0;
+    for (int i = 0; i < s.Length; i++) {
+        if (!char.IsNumber(s[i])){
+            var number = Convert.ToInt32(s.Substring(lastNumberIndex, i - lastNumberIndex));
+            Console.WriteLine(number);
+            result.Append(s[i], number);
+            lastNumberIndex = i + 1;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    return result.ToString();
+}
+
+Console.WriteLine(uncompress("2c3a1t"));
